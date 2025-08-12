365706b8749ce29b66b8357f9c0b72f91f5669c8
userCheck <eopolchentseva@gmail.com>
2025-08-12 18:15:25 +0300

    morgan

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,17 @@ import express from "express"
 import {PORT} from "./config/libConfig.js";
 import {libRouter} from "./routes/libRouter.ts";
 import {errorHandler} from "./errorHandler/errorHandler.ts";
+import morgan from "morgan";
+import * as fs from "node:fs";
 
 export const launchServer = () => {
     const app = express();
     app.listen(PORT, () => console.log(`Server runs at http://localhost:${PORT}`));
-
+    const logStream = fs.createWriteStream('logs.txt', {flags: "a"});
     //=========Middleware=============
     app.use(express.json());
-
+    app.use(morgan("dev"));
+    app.use(morgan('combined', {stream: logStream}));
 
     //=============Router=============
 
