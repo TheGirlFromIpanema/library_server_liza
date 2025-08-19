dfeeba4fc8848acfc8affcd4ce4e83d65dac3b23
userCheck <eopolchentseva@gmail.com>
2025-08-19 13:42:24 +0300

    Added Pick_up and ReturnBook methods

--- src/services/libServiseImplSQL.ts
+++ src/services/libServiseImplSQL.ts
@@ -2,6 +2,8 @@ import {LibService} from "./libService.js";
 import {Book, BookGenres, BookStatus} from "../model/Book.js";
 import {pool} from "../config/libConfig.js";
 import {HttpError} from "../errorHandler/HttpError.js";
+import { v4 as uuidv4 } from 'uuid';
+import {User} from "../model/User.js";
 
 export class LibServiceImplSQL implements LibService {
 
@@ -24,8 +26,30 @@ export class LibServiceImplSQL implements LibService {
         return Promise.resolve(result as Book[]);
     }
 
-    pickUpBook(id: string, reader: string): Promise<void> {
-        return Promise.resolve(undefined);
+    async pickUpBook(id: string, reader: string): Promise<void> {
+        const [result] = await pool.query('SELECT * FROM books WHERE id = ?', [id]);
+        const books = result as Book[];
+        const [userResult] = await pool.query('SELECT * FROM readers WHERE name = ?', [reader]);
+        const users = userResult as User[];
+        if (books.length !== 0) {
+            const book = books[0];
+            if (book.status !== BookStatus.ON_STOCK)
+                throw new HttpError(409, "Book already on hand");
+            let userID = '';
+            if (users.length === 0){
+                userID = uuidv4();
+                await pool.query('INSERT INTO readers VALUES(?,?)',
+                    [userID, reader]);
+            }
+            else {
+                const user = users[0];
+                userID = user.id;
+            }
+            await pool.query('INSERT INTO books_readers (book_id, reader_id, pick_date) VALUES(?,?,?)',
+                [id, userID, new Date()]);
+            await pool.query('UPDATE books SET status = "on_hand" WHERE id = ?', [id]);
+        } else
+            throw new HttpError(400, "Book not found");
     }
 
     async removeBook(id: string): Promise<Book> {
@@ -39,8 +63,18 @@ export class LibServiceImplSQL implements LibService {
             throw new HttpError(400, "Book not found");
     }
 
-    returnBook(id: string): Promise<void> {
-        return Promise.resolve(undefined);
+    async returnBook(id: string): Promise<void> {
+        const [result] = await pool.query('SELECT * FROM books WHERE id = ?', [id]);
+        const books = result as Book[];
+        if (books.length !== 0) {
+            const book = books[0];
+            if (book.status !== BookStatus.ON_HAND)
+                throw new HttpError(409, "Book already on stock");
+            await pool.query('UPDATE books_readers SET return_date = ? WHERE book_id = ?',
+                [new Date(), id]);
+            await pool.query('UPDATE books SET status = "on_stock" WHERE id = ?', [id]);
+        } else
+            throw new HttpError(400, "Book not found");
     }
 
     async getBooksByGenreAndStatus(genre: BookGenres, status: BookStatus): Promise<Book[]> {
