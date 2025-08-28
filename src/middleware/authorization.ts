68c2d34f1c9afdee4de392b2c2b178f252a150b5
userCheck <eopolchentseva@gmail.com>
2025-08-28 13:37:26 +0300

    refactor checkAccountById

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -14,16 +14,16 @@ export const authorize = (pathRoute:Record<string, Roles[]>)=>
         const roles = req.roles;
         if(!roles || roles?.some(r => pathRoute[route].includes(r)))
             next();
-        else throw new HttpError(403, "")
+        else throw new HttpError(403, "authorize block")
     }
 
 export  const checkAccountById = (checkPathId:string[]) => {
     return (req:AuthRequest, res:Response, next:NextFunction)=> {
         const route = req.method + req.path;
         const roles = req.roles;
-        if(!roles || !checkPathId.includes(route) || (!req.roles!.includes(Roles.ADMIN)
-            && req.roles!.includes(Roles.USER)
-            && req.userId == req.query.id))
+        if(!roles || !checkPathId.includes(route) || req.roles!.includes(Roles.ADMIN) ||
+            (req.roles!.includes(Roles.USER)
+            && (req.body? req.userId == req.body.id : req.userId == req.query.id)))
             next();
         else throw new HttpError(403, "You can modify only your own account")
     }
